bbce4e0644a8ff860720aa90ad075dd9b9727583
davidyu7 <david.yu@yale.edu>
2020-06-30 11:16:58 -0400

    completed User-Transmission relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+
+
     belongs_to :res_college
     has_many :tests
     has_many :visits
     has_many :locations, through: :visits
     has_many :spreading_transmissions, foreign_key: :spreader_id, class_name: 'Transmission'
     has_many :contracting_transmissions, foreign_key: :infectee_id, class_name: 'Transmission'
-    has_many :spreaders, through: :contracting_relationships
-    has_many :infectees, through: :spreading_relationships
+    has_many :spreaders, through: :contracting_transmissions
+    has_many :infectees, through: :spreading_transmissions
 end
