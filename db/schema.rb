3be065630b0f7a5946950da0116e10ea2024f589
davidyu7 <david.yu@yale.edu>
2020-06-30 14:29:57 -0400

    fixed merge conflict again

--- db/schema.rb
+++ db/schema.rb
@@ -41,14 +41,9 @@ ActiveRecord::Schema.define(version: 2020_06_29_182917) do
   end
 
   create_table "transmissions", force: :cascade do |t|
-    t.integer "origin_id"
-<<<<<<< HEAD
-    t.integer "new_patient_id"
     t.integer "location_id"
-=======
     t.integer "spreader_id"
     t.integer "infectee_id"
->>>>>>> dave
     t.datetime "date"
     t.boolean "confirmed"
     t.datetime "created_at", precision: 6, null: false
@@ -59,11 +54,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_182917) do
     t.string "name"
     t.integer "class_year"
     t.integer "res_college_id"
-<<<<<<< HEAD
-    t.string "type"
-=======
     t.string "user_type"
->>>>>>> dave
     t.boolean "contagious"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -80,7 +71,3 @@ ActiveRecord::Schema.define(version: 2020_06_29_182917) do
   end
 
 end
-<<<<<<< HEAD
-
-=======
->>>>>>> dave
