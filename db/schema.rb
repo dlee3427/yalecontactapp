736ae2b60a18c78fc70d9ce39d13fef57a902178
davidyu7 <david.yu@yale.edu>
2020-06-30 12:28:26 -0400

    generated data using seed file

---
+++ db/schema.rb
@@ -0,0 +1,73 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_06_29_182917) do
+
+  create_table "facilities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "building"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "res_colleges", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "date"
+    t.boolean "result"
+    t.integer "facility_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transmissions", force: :cascade do |t|
+    t.integer "origin_id"
+    t.integer "spreader_id"
+    t.integer "infectee_id"
+    t.datetime "date"
+    t.boolean "confirmed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "class_year"
+    t.integer "res_college_id"
+    t.string "user_type"
+    t.boolean "contagious"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "location_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.boolean "contagious"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+["ES", "MC", "MY", "BF"].each do |rc|
+    ResCollege.create(name: rc)
+end
+
+10.times do
+    User.create(
+        name: Faker::Name.name,
+        class_year: rand(2021..2024),
+        res_college: ResCollege.all.sample,
+        user_type: ["student", "student", "faculty"].sample
+    )
+end
+
+toads = Location.create(name: "Toad's Place", building: "Off-Campus")
+sss = Location.create(name: "SSS 114", building: "SSS")
+dorm = Location.create(name: "ES-A22", building: "Ezra Stiles College")
+
+
+first_day = Time.new(2020, 9, 1)
+last_day = first_day + 3.days
+
+100.times do
+    start_time = Faker::Time.between_dates(from: first_day, to: last_day, period: :night)
+    end_time = start_time + rand(2..3).hours
+    Visit.create(
+        user: User.all.sample,
+        location: toads,
+        start_time: start_time,
+        end_time: end_time
+    )
+end
+
+50.times do
+    start_time = Faker::Time.between_dates(from: first_day, to: last_day, period: :day)
+    end_time = start_time + rand(1..2).hours
+    Visit.create(
+        user: User.all.sample,
+        location: sss,
+        start_time: start_time,
+        end_time: end_time
+    )
+end
+
+50.times do
+    start_time = Faker::Time.between_dates(from: first_day, to: last_day, period: :all)
+    end_time = start_time + rand(2..7).hours
+    Visit.create(
+        user: User.all.sample,
+        location: dorm,
+        start_time: start_time,
+        end_time: end_time
+    )
+end
